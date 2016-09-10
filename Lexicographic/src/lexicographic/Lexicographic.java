f6b0d0196ea365818b405915b29702df7e47d2e5
BELTRAN AQUINO <BRAQUINO@fit.edu.ph>
2016-09-10 20:39:47 +0800

    Coded Lexi

--- Lexicographic/src/lexicographic/Lexicographic.java
+++ Lexicographic/src/lexicographic/Lexicographic.java
@@ -4,7 +4,10 @@ package lexicographic;
 public class Lexicographic {
 
     public static void main(String[] args) {
-        
+        String charset = "ABC";
+        for (int i =0; i <charset.length();i++){
+            System.out.println(charset.charAt(i));
+        }
     }
     
 }
