6de828845f47b4cb449a191c4955095c38efc8f2
braquino <braquino@192.168.56.1>
2016-09-17 19:09:21 +0800

    Added 123h

--- Lexicographic/src/lexicographic/Lexicographic.java
+++ Lexicographic/src/lexicographic/Lexicographic.java
@@ -2,7 +2,7 @@
 package lexicographic;
 
 public class Lexicographic {
-
+//123h
     public static void main(String[] args) {
         String charset = "ABC";
         for (int i =0; i <charset.length();i++){
