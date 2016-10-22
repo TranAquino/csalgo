bfe9cb46e47956110b7260de22d2ef82f2d83bc2
Beltran Aquino <tranaquino@gmail.com>
2016-10-22 20:10:36 +0800

    I FIXED IT

---
+++ Lexicographic.java
@@ -0,0 +1,18 @@
+
+package Activities;
+
+public class Lexicographic {
+// HE:::PO
+    public static void main(String[] args) {
+        String charset = "ABC";
+        /*for (int i =0; i <charset.length();i++){
+            System.out.println(charset.charAt(i));
+        }
+        */
+        int arr[]= {5,22,1,6,9};
+        for (int i:arr){
+            System.out.println(i + " ");
+    }
+    }
+    
+}
